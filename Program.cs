ccb49c073f8f152d5c102724e8f6887853674fa0
nidal <nidalcheruvadi@gmail.com>
2025-07-29 16:57:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace StringTask
+{
+    public class Program
+    {
+        //public string word = "Bananas";
+        static void Main(string[] args)
+        {
+            var a=new Converter();
+             a.change("Bananas");
+
+        }
+    }
+
+
+    public class  Converter
+    {
+        public void change(string value) 
+        {
+          var arr =  value.ToArray();
+
+           var newArr = arr.Reverse().Where(x=>x.ToString().Length %2 ==0).ToString();
+
+            Console.WriteLine(newArr);
+
+        }
+    }
+}
